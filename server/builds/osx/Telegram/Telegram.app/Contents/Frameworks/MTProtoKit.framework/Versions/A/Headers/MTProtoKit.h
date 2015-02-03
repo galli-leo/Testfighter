3fdfec27cb60184316bc10de24aae5194f80fc26
DJLectr0 <leonardogalli@leonardos-macbook-pro.home>
2015-02-03 20:09:53 +0100

    Removed Telegram

--- server/builds/osx/Telegram/Telegram.app/Contents/Frameworks/MTProtoKit.framework/Versions/A/Headers/MTProtoKit.h
+++
@@ -1,9 +0,0 @@
-#ifndef MtProtoKit_MTProtoKit_h
-#define MtProtoKit_MTProtoKit_h
-
-#import <MTProtoKit/MTContext.h>
-#import <MTProtoKit/MTProto.h>
-#import <MTProtoKit/MTRequestMessageService.h>
-#import <MTProtoKit/MTRequest.h>
-
-#endif
