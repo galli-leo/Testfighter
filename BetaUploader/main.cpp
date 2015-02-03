adcd564834df81c475019d3f079a43b4c71e8f69
DJLectr0 <leonardogalli@leonardos-macbook-pro.home>
2015-02-03 19:55:50 +0100

    Finished feed implementation
    - Mac uploader now working but only with cmd

--- BetaUploader/main.cpp
+++ BetaUploader/main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
+#include "uploadwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //CMD options: [dir] [os]
+    QStringList args = a.arguments();
+    //qDebug() << "args" << args[2];
+    if(args.count() > 2){
+        //We are launching from cmd
+        UploadWindow* uw = new UploadWindow();
+        uw->os = args[2];
+        uw->dir = args[1];
+        uw->show();
+        uw->startUpload();
+    }else{
+        MainWindow w;
+        w.show();
+    }
 
     return a.exec();
 }
--- BetaUploader/uploadwindow.cpp
+++ BetaUploader/uploadwindow.cpp
@@ -19,18 +19,9 @@ UploadWindow::UploadWindow(QWidget *parent) :
     // Lets print the HTTP GET response.
     //qDebug( reply->readAll());
 
-    QNetworkReply *replyList = networkMgr->get( QNetworkRequest( QUrl( AppData::Instance()->settings["url"].toString() + "list.json" ) ) );
 
-    QEventLoop loopList;
-    QObject::connect(replyList, SIGNAL(finished()), &loopList, SLOT(quit()));
-
-    // Execute the event loop here, now we will wait here until readyRead() signal is emitted
-    // which in turn will trigger event loop quit.
-    loopList.exec();
-
-    QJsonDocument loadDocList = QJsonDocument::fromJson(replyList->readAll());
     QJsonDocument loadDoc = QJsonDocument::fromJson(reply->readAll());
-    this->list = loadDocList.object();
+
     this->options = loadDoc.object();
     //setupFields();
 
@@ -42,6 +33,19 @@ UploadWindow::~UploadWindow()
 }
 void UploadWindow::startUpload()
 {
+    //Get the list for the os we are uploading
+    QNetworkAccessManager *networkMgr = new QNetworkAccessManager(this);
+    QNetworkReply *replyList = networkMgr->get( QNetworkRequest( QUrl( AppData::Instance()->settings["url"].toString() + this->os+"_list.json" ) ) );
+    qDebug() << os << AppData::Instance()->settings["url"].toString() + this->os+"_list.json";
+    QEventLoop loopList;
+    QObject::connect(replyList, SIGNAL(finished()), &loopList, SLOT(quit()));
+
+    // Execute the event loop here, now we will wait here until readyRead() signal is emitted
+    // which in turn will trigger event loop quit.
+    loopList.exec();
+
+    QJsonDocument loadDocList = QJsonDocument::fromJson(replyList->readAll());
+    this->list = loadDocList.object();
 
     setupFields();
     initUpload();
@@ -61,11 +65,7 @@ void UploadWindow::setupFields()
         dirName = dir.split("/").at(dir.split("/").count()-2);
 
     }
-#ifdef Q_OS_MAC
-    //For the time beeing yosemite does not work so we just use a set path name
-    dirName = "test";
 
-#endif
     qDebug() << this->list;
     qDebug() << dir;
     qDebug() << dirName;
@@ -156,13 +156,9 @@ void UploadWindow::initUpload()
         dirName = dir.split("/").at(dir.split("/").count()-2);
 
     }
-#ifdef Q_OS_MAC
-    //For the time beeing yosemite does not work so we just use a set path name
-    dirName = "test";
-    QDirIterator it("/Users/leonardogalli/BetaLauncher/BetaUploader/test", QDir::Files, QDirIterator::Subdirectories);
-#else
+
     QDirIterator it(this->dir, QDir::Files, QDirIterator::Subdirectories);
-#endif
+
     QNetworkReply *replyHash = networkMgr->get( QNetworkRequest( QUrl( AppData::Instance()->settings["url"].toString() + "builds/hash"+ dirName + ".json" ) ) );
 
     QEventLoop loopHash;
@@ -284,12 +280,15 @@ void UploadWindow::buildSubmit()
          dirName = dir.split("/").at(dir.split("/").count()-2);
 
      }
-#ifdef Q_OS_MAC
-     dirName = "test";
-#endif
+
      qDebug() << dirName.toLocal8Bit();
      fileName.setBody(dirName.toLocal8Bit());
      multiPart->append(fileName);
+     QHttpPart osName;
+     osName.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"os\""));
+     osName.setBody(this->os.toLocal8Bit());
+     multiPart->append(osName);
+
     qDebug() << multiPart;
     QNetworkAccessManager* networkManager = new QNetworkAccessManager(this);
     QNetworkReply* reply = networkManager->post(request, multiPart);
