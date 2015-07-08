6f1cced629b2735b52275d7bfcda24bed21646e1
DJLectr0 <leonardogalli@leonardos-macbook-pro.home>
2015-07-08 15:30:16 +0200

    - Fixed removing unessecary files

--- server/remove_files.php
+++ server/remove_files.php
@@ -1,13 +1,18 @@
 <?php
+	
 if($_POST["password"]=="testfighter2015"){
+	$os = $_POST["os"];
 	$files_str = $_POST['files'];
 	$files = explode(";", $files_str);
+	var_dump($_POST);
 	foreach ($files as $file){
+		
 		if($file != ""){
-			chmod($file,0755); //Change the file permissions if allowed
-    		unlink($file); //remove the file
+			chmod("builds/$os/$file",0755); //Change the file permissions if allowed
+    		unlink("builds/$os/$file"); //remove the file
+    		echo "Succesfully removed file: builds/$os/$file\n";
 		}
 	}
-	echo "Succesfully removed files";
+	
 }
 ?>
