e1f7f507983e719806e3d6a35c8bd56c0841da39
DJLectr0 <leonardogalli@leonardos-macbook-pro.home>
2015-07-09 11:42:34 +0200

    Fixed empty folder after removal of files
    Empty folders are now deleted

--- server/remove_files.php
+++ server/remove_files.php
@@ -1,6 +1,72 @@
 <?php
+ini_set('display_errors',1);
+
+
+function doesDirectoryOnlyContainEmptyFolders($path) {
+  if(is_dir($path)) {
+    $directoryContent = scandir($path);
+    if (count($directoryContent) <= 2) {
+      return true;
+    }
+    else {
+      foreach ($directoryContent as $subPath) {
+        if($subPath !== '.' && $subPath !== '..' && !doesDirectoryOnlyContainEmptyFolders("$path/$subPath")) {
+	        echo "Subpath not empty: $path $subPath";
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  echo "Found file for path: $path";
+  return false;
+}
+
+function deleteDirectory($dir) {
+    if (!file_exists($dir)) {
+        return true;
+    }
+
+    if (!is_dir($dir)) {
+        return unlink($dir);
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+
+        if (!deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+            return false;
+        }
+
+    }
+
+    return rmdir($dir);
+}
 	
+function deleteEmptyFoldersRecursivelyUpAndDown($path) {
+  if (is_dir($path)) {
+    if(doesDirectoryOnlyContainEmptyFolders($path)) {
+      deleteDirectory($path);
+      echo "Deleted empty folder: $path<br>";
+      /*$parentFolder = substr($path, 0, strripos ($path, '/'));
+      deleteEmptyFoldersRecursivelyUpAndDown($parentFolder);*/
+    }
+    else {
+      $directoryContent = scandir($path);
+      foreach ($directoryContent as $subPath) {
+	      if($subPath !== '.' && $subPath !== '..'){
+	      	echo "Searching folders: $subPath<br>";
+		  	deleteEmptyFoldersRecursivelyUpAndDown("$path/$subPath");
+        }
+      }
+    }
+  }
+}	
+
 if($_POST["password"]=="testfighter2015"){
+	echo("test");
 	$os = $_POST["os"];
 	$files_str = $_POST['files'];
 	$files = explode(";", $files_str);
@@ -13,6 +79,12 @@ if($_POST["password"]=="testfighter2015"){
     		echo "Succesfully removed file: builds/$os/$file\n";
 		}
 	}
-	
+	//var_dump(doesDirectoryOnlyContainEmptyFolders("builds/$os/"));
+	//var_dump(doesDirectoryOnlyContainEmptyFolders("builds/$os/JumpNoMore/Binaries/"));
+	deleteEmptyFoldersRecursivelyUpAndDown("builds/$os");
+
+
+
+
 }
 ?>
